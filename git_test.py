ca9015f103796b05db09c3d4f743eeecd1a397fa
AtomXF <laurent.friob@gmail.com>
2020-01-19 11:09:25 +0100

    add python code

---
+++ git_test.py
@@ -0,0 +1,13 @@
+from math import sin
+print("Hello Git world, Linus is listening")
+say = input("Say something, anything ")
+print("Good boy")
+print(say)
+
+def sine():
+    print("f(x)= sin(x), where x is in degrees")
+    x= input("What angle? ")
+    angle = float(x)
+    return print(sin(angle))
+
+sine()
